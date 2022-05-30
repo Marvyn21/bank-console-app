2adea94db99f9424b11b18d2211ad836abbad6ba
Marvyn21 <marvynrogony@gmail.com>
2022-05-30 18:34:28 +0300

    first commit

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Ken", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}. ");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            //account.makeDeposits(-300, DateTime.Now, "yetss");
+        }
+    }
+}
